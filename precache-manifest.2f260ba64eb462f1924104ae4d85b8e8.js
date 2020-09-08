79e0bed5b88310c267f86cb60b8618a7a4812340
MitBit-Dev <miteshrajput.dev@gmail.com>
2020-09-08 16:20:46 +0530

    Updates

---
+++ precache-manifest.2f260ba64eb462f1924104ae4d85b8e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276d4ceb7d69ffa9fb247dd4e6cf44aa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "868622210aa3895246eb",
+    "url": "/portfolio/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "6a8b0144103319e8e128",
+    "url": "/portfolio/static/css/main.116a3809.chunk.css"
+  },
+  {
+    "revision": "868622210aa3895246eb",
+    "url": "/portfolio/static/js/2.cdcec7a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.cdcec7a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8b0144103319e8e128",
+    "url": "/portfolio/static/js/main.92861be0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
